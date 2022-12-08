ed20d4f6d85901558d13c2bb07af1caec46e7447
abordones <allmaxbordones@gmail.com>
2022-12-07 22:36:33 -0300

    el resto de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="https://cdn.discordapp.com/attachments/768126732955549757/996963521294250015/unknown.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ComponenteUno></ComponenteUno>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ComponenteUno from './components/ComponenteUno.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ComponenteUno
   }
 }
 </script>
---
+++ src/components/ComponenteUno.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="componente-uno">
+    <hr/>        
+        <h1>XDDDDDDDD</h1>
+        <h2>{{sub}}</h2>
+    <hr/> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponenteUno',
+    data() {
+        return {
+            sub: 'soy un subtitulo sis'
+        }
+    }
+}
+
+</script>
