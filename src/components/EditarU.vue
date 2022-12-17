f7ac465dff8fbf318767eb952925665591c205bf
abordones <allmaxbordones@gmail.com>
2022-12-17 15:53:53 -0300

    cambios componentes

---
+++ src/components/EditarU.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        El kiwi es gay
+    </div>
+
+</template>
+
+
+<script>
+export default{
+
+    
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+import CrearU from '../components/CrearU.vue'
+import EditarU from '../components/EditarU.vue'
+import ListarU from '../components/ListarU.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+
+  {
+    path: '/editar',
+    name: 'Editar',
+    component: EditarU
+  },
+
+  {
+    path: '/crear',
+    name: 'Crear',
+    component: CrearU
+  },
+
+  {
+    path: '/listar',
+    name: 'Listar',
+    component: ListarU
+  },
+
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
